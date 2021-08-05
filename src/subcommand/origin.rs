d0f6ff834e338665b2f60ce629197ed46e7d63fc
K_atc <shiftx1026@gmail.com>
2021-08-05 14:13:16 +0900

    Update test_find_origin_of to follow current result

--- src/subcommand/origin.rs
+++ src/subcommand/origin.rs
@@ -100,8 +100,11 @@ impl fmt::Display for Origin {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         write!(
             f,
-            "Offset {:#x} derives from node \"{}\" by {})",
-            self.of_offset, self.node, self.chunk
+            "Offset {:#x} derives from {}(position={:#x}) of node \"{}\"",
+            self.of_offset,
+            self.chunk.name(),
+            self.position,
+            self.node,
         )
     }
 }
@@ -169,4 +172,84 @@ fn find_origin_of(offset: usize, seeds_dir: &Path, seeds: &Vec<Sha1String>) -> O
         }
     }
     None
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::mutation_graph::parser::parse_mutation_graph_file;
+    use crate::mutation_graph::sha1_string::Sha1String;
+    use crate::subcommand::origin::find_origin_of;
+    use std::path::Path;
+
+    #[test]
+    fn test_find_origin_of() {
+        let seeds_dir = Path::new("test/sample/seeds/fuzzer-test-suite-openssl-1.0.1f/");
+        let graph = parse_mutation_graph_file(Path::new(
+            "test/sample/mutation_graph_file/fuzzer-test-suite-openssl-1.0.1f.dot",
+        ))
+        .unwrap();
+        let seeds = graph
+            .self_and_its_predecessors_of(&Sha1String::from(
+                "c298122410da09836c59484e995c287294c31394",
+            ))
+            .unwrap()
+            .iter()
+            .filter(|name| seeds_dir.join(&name).exists())
+            .map(|v| Sha1String::from(v.clone()))
+            .collect();
+
+        // On far node from target node
+        assert_eq!(
+            find_origin_of(0x13, seeds_dir, &seeds).unwrap().node,
+            Sha1String::from("99878cf124782dc6d21f079bb29e0dba54606bbb")
+        );
+        assert_eq!(
+            find_origin_of(0x13, seeds_dir, &seeds).unwrap().position,
+            0x1d
+        );
+        assert_eq!(
+            find_origin_of(0x14, seeds_dir, &seeds).unwrap().position,
+            0x1e
+        );
+        assert_eq!(
+            find_origin_of(0x16, seeds_dir, &seeds).unwrap().position,
+            0x25
+        );
+        assert_eq!(
+            find_origin_of(0x17, seeds_dir, &seeds).unwrap().position,
+            0x26
+        );
+        assert_eq!(
+            find_origin_of(0x1c, seeds_dir, &seeds).unwrap().position,
+            0x29
+        );
+
+        // On in front of target node
+        assert_eq!(
+            find_origin_of(0x15, seeds_dir, &seeds).unwrap().node,
+            Sha1String::from("76e46ec1efcdcb854486037defc3e777a62524ed")
+        );
+        assert_eq!(
+            find_origin_of(0x15, seeds_dir, &seeds).unwrap().position,
+            0x15
+        );
+        assert_eq!(
+            find_origin_of(0x18, seeds_dir, &seeds).unwrap().position,
+            0x18
+        );
+        assert_eq!(
+            find_origin_of(0x19, seeds_dir, &seeds).unwrap().position,
+            0x19
+        );
+
+        // On target node
+        assert_eq!(
+            find_origin_of(0x1a, seeds_dir, &seeds).unwrap().node,
+            Sha1String::from("c298122410da09836c59484e995c287294c31394")
+        );
+        assert_eq!(
+            find_origin_of(0x1a, seeds_dir, &seeds).unwrap().position,
+            0x1a
+        );
+    }
 }
